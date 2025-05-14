024d51dbc55c9d9e3d10eebca56e46e170938bee
Tom Denley <t.denley@strategic-blue.com>
2025-05-14 10:50:04 +0100

    read accelerometer data

---
+++ sketch/sketch.ino
@@ -0,0 +1,48 @@
+
+
+#include <Adafruit_LIS3DH.h>
+#include <Adafruit_NeoPixel.h>
+
+
+#define I2C_ADDRESS 0x18
+Adafruit_LIS3DH lis = Adafruit_LIS3DH();
+
+#define LED_COUNT 10
+Adafruit_NeoPixel strip(LED_COUNT, PIN_EXTERNAL_NEOPIXELS, NEO_GRB + NEO_KHZ800);
+
+
+void setup() {
+
+  Serial.begin(115200);
+  while (!Serial) delay(10);
+
+  Serial.println("start");
+
+    
+  if (!lis.begin(I2C_ADDRESS)) {
+    Serial.println("Couldnt find accelerometer");
+    while (1) yield();
+  }
+  Serial.println("accelerometer found!");
+  
+  // lis.setRange(LIS3DH_RANGE_4_G);   // 2, 4, 8, or 16
+  // lis.setPerformanceMode(LIS3DH_MODE_NORMAL); // LOW_POWER, NORMAL, or HIGH_RESOLUTION
+  // lis.setDataRate(LIS3DH_DATARATE_50_HZ); // 1, 20, 25, 50, 100, 200, 400, POWERDOWN, LOWPOWER_5HZ, LOWPOWER_1K6HZ
+
+
+
+
+}
+
+void loop() {
+  sensors_event_t event;
+  lis.getEvent(&event);
+  Serial.print("\t\tX: "); Serial.print(event.acceleration.x);
+  Serial.print(" \tY: "); Serial.print(event.acceleration.y);
+  Serial.print(" \tZ: "); Serial.print(event.acceleration.z);
+  Serial.println(" m/s^2 ");
+
+
+
+
+}
