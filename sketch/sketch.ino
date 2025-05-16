1e2243cd40604539aca2f445de23542cc57b7294
Tom Denley <t.denley@strategic-blue.com>
2025-05-16 15:56:48 +0100

    get a rainbow display going

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -10,23 +10,28 @@ Adafruit_LIS3DH lis = Adafruit_LIS3DH();
 Adafruit_NeoPixel strip(LED_COUNT, PIN_EXTERNAL_NEOPIXELS, NEO_GRB + NEO_KHZ800);
 
 
-unsigned long previousPixelChangeTime = 0;    // when did we last change a pixel
-int           pixelChangeIntervalMillis = 50; // how many millis between pixel changes
+unsigned long previousPixelChangeTime = 0;     // when did we last change a pixel
+int           pixelChangeIntervalMillis = 50;  // how many millis between pixel changes
 
-unsigned long previousDataSampleTime = 0;     // when did we last sample data
-int           dataSampleIntervalMillis = 500; // how many millis between data samples
+unsigned long previousDataSampleTime = 0;      // when did we last sample data
+int           dataSampleIntervalMillis = 500;  // how many millis between data samples
 
-unsigned long previousDataWriteTime = 0;      // when did we last write data
-int           dataWriteIntervalMillis = 500;  // how many millis between data writes
+unsigned long previousDataWriteTime = 0;       // when did we last write data
+int           dataWriteIntervalMillis = 10000; // how many millis between data writes
 
 
 void setup() {
+  pinMode(PIN_EXTERNAL_POWER, OUTPUT);
+  digitalWrite(PIN_EXTERNAL_POWER, HIGH);
+  strip.begin();
+  strip.show();
+  strip.setBrightness(50);
+
   Serial.begin(115200);
   while (!Serial) delay(10);
 
   Serial.println("start");
 
-    
   if (!lis.begin(I2C_ADDRESS)) {
     Serial.println("Couldnt find accelerometer");
     while (1) yield();
@@ -60,9 +65,10 @@ void loop() {
   }
 }
 
-
 void changePixel() {
-
+  pixelChangeIntervalMillis = 10;
+  rainbow();
+  strip.show();
 }
 
 void sampleData() {
@@ -75,11 +81,38 @@ void sampleData() {
 }
 
 void writeData() {
-  if (okay-to-write) {
+  bool okayToWrite = true;
+  if (okayToWrite) {
       noInterrupts();
       File f = LittleFS.open("littlefsfile.csv", "a");
-      f.printf("%d,%d,%d\n", data1, data2, data3);
+      f.printf("%d,%d,%d\n", 0.1, 0.2, 0.3);
       f.close();
       interrupts();
   }
+}
+
+#define RAINBOW_STATES 765 // 255 states * 3 colours
+#define RAINBOW_STEP 5
+void rainbow() {
+  static uint16_t cyclePosition = 0;
+
+  for(uint16_t pixelIdx = 0; pixelIdx < LED_COUNT; pixelIdx++) {
+    strip.setPixelColor(pixelIdx, calculateRainbowColour(pixelIdx * RAINBOW_STEP + cyclePosition));
+  }
+
+  cyclePosition = (cyclePosition + RAINBOW_STEP) % RAINBOW_STATES;
+}
+
+uint32_t calculateRainbowColour(uint16_t position) {
+  uint8_t phase = (position % RAINBOW_STATES) / 255; // 0=RG; 1=GB; 2=RB
+  uint8_t increasingComponent = position % 255;
+  uint8_t decreasingComponent = 255 - increasingComponent;
+
+  if (phase == 0) {
+    return strip.Color(decreasingComponent, increasingComponent, 0);
+  }
+  if (phase == 1) {
+    return strip.Color(0, decreasingComponent, increasingComponent);
+  }
+  return strip.Color(increasingComponent, 0, decreasingComponent);
 }
